883bf46226514fd925f75b2a171e26916559c247
Gabriel Charlui Correa <gabrielcharlui@hotmail.com>
2024-05-11 22:01:33 -0300

    refactor: remove comments

--- script.js
+++ script.js
@@ -1,20 +1,20 @@
 document.addEventListener('DOMContentLoaded', (event) => {
-   const centerTitle = document.querySelector('.center h1'); // Seleciona o título dentro da div 'center'
-   let fontSize = 1.2; // Tamanho inicial da fonte
-   let increment = true; // Controla a direção da animação
+   const centerTitle = document.querySelector('.center h1');
+   let fontSize = 1.2;
+   let increment = true;
 
    function animateTitle() {
-      if (fontSize >= 2) { // Define o tamanho máximo da fonte
+      if (fontSize >= 2) {
          increment = false;
-      } else if (fontSize <= 1.2) { // Define o tamanho mínimo da fonte
+      } else if (fontSize <= 1.2) {
          increment = true;
       }
 
-      fontSize += increment ? 0.01 : -0.01; // Aumenta ou diminui o tamanho da fonte
-      centerTitle.style.fontSize = `${fontSize}em`; // Aplica o novo tamanho da fonte
+      fontSize += increment ? 0.01 : -0.01;
+      centerTitle.style.fontSize = `${fontSize}em`;
 
-      requestAnimationFrame(animateTitle); // Continua a animação
+      requestAnimationFrame(animateTitle);
    }
 
-   animateTitle(); // Inicia a animação
+   animateTitle();
 });
